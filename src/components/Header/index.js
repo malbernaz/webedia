bf8475c947acfc35f14c84752aa7ae9722cab02f
malbernaz <albernazmiguel@gmail.com>
2017-06-29 12:53:57 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import "./Header.css";
+
+import React from "react";
+
+const Header = () =>
+  <header className="Header">
+    <div className="Header__top">
+      um site do
+      <h1 className="logo">
+        web<span>edia.</span>
+      </h1>
+      group
+    </div>
+    <nav className="Header__nav">
+      <a>lorem ipsum</a>
+      <a>lorem ipsum</a>
+      <a>lorem ipsum</a>
+      <a>lorem ipsum</a>
+      <a>lorem ipsum</a>
+      <a>lorem ipsum</a>
+      <a>lorem ipsum</a>
+      <a>lorem ipsum</a>
+    </nav>
+  </header>;
+
+export default Header;
